a52e399314559de81a692c74447593a7e8855eb8
root <root@chatbot-node.c.dev-happierhr.internal>
2017-06-13 11:23:20 +0000

    first cut bot

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports.ssl = {
+	keyPath: 'key.pem',
+	certPath: 'key-cert.pem'
+};
+
+module.exports.VERIFY_TOKEN="this_is_my_token";
+
+module.exports.PAGE_ACCESS_TOKEN = "EAAGliJ4kNuEBALEQ2ZCE336Y4eAAt9kDYL3drhUwK7LzpeYjeMh3bRSNSXmMTnueZA4UjcBVL5Icj3AVy6XutpHZCDvIrT4cIjLGc1F8K74JpQOoNUOGVIlk6xPiYv68IVluSWqOsyycohgHbIFj6ancQYNTPZC7z0yoyYfLFwZDZD";
+
+module.exports.securePort = 8080;
