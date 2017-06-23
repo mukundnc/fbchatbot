bd2963472e9471a7f29732dfcdb9d817c6986d41
mukundnc <mukund.nc@gmail.com>
2017-06-23 13:00:35 +0530

    removed keys

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports.ssl = {
 	certPath: 'key-cert.pem'
 };
 
-module.exports.VERIFY_TOKEN="mukundan_nc_secret";
+module.exports.VERIFY_TOKEN="your_secret";
 
-module.exports.PAGE_ACCESS_TOKEN = "DQVJ1V0RFZAExTVHhxSHd0ZADV0LW9fdS1Na1JjM09GMGtlcFRFUFQ3N2dld3ZAsUlRpekxkeTAtSTdKSm9xanFwSTJMLVJJZAmRTdE95RGxxVjNqNDJFc0hZAdlZAZARy11YnNjVWpuSzBBQ2ljbUlNT29QMXhqekZAJMm9Da3JOT282QlBUU2VGREVmd29nYk9BRTRqSU5MdV9XQlRNTEVGcEs2UFp5UlIzZA2ZABMFRiS3lUaW1yQlBaRzhrUGMyc1ZAvUkhwN3FHd2pFVkdzVXRhVFdFSAZDZD";
+module.exports.PAGE_ACCESS_TOKEN = "your_access_token";
 
 module.exports.securePort = 8080;
